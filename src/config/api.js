240914923650c6f9384b4ade28b9eb4d2fe67e7b
Vignesh-venkat <55269636+Vignesh-venkat@users.noreply.github.com>
2019-11-23 13:26:48 +0530

    added env config

--- src/config/api.js
+++ src/config/api.js
@@ -2,9 +2,9 @@ export const cors_url = "https://cors-anywhere.herokuapp.com/"
 
 export const environment = {
     local: "http://localhost:3002",
-    prod: "https://usermanagement-node.herokuapp.com"
+    dev: "https://usermanagement-node.herokuapp.com"
 }
-const api = environment.local
+const api =environment[process.env.CONN_ENV] || environment.local
 export const apiList = {
     login_POST: api + "/user/login",
     logout_POST: api + "/user/logout",
